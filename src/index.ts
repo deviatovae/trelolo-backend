e5e64e2c54c6a37fa7101f4042b9696306c81f40
Alena Deviatova <aedeviatova@yandex.ru>
2023-02-14 00:08:54 +0300

    feat: fix middleware

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,9 @@ app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
 
-app.use((req) => {
+app.use((req, res, next) => {
     console.log(`[${req.method}] ${req.url}`);
+    next();
 });
 app.use(responseTime((req, res, time) => {
     console.log(`[${req.method || '?'}] ${req.url || '?'} - ${(time / 1000).toFixed(3)}`);
